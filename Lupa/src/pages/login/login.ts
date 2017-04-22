c6c913064b48ec4e22aeaddd1f03daabe6e8f2c7
stevenlouback <sklcarvalho@agropecuariaipe.int>
2017-04-22 17:46:57 -0300

    first commit

---
+++ Lupa/src/pages/login/login.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the Login page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class Login {
+
+
+}
